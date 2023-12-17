e23f266033a410dc91ca78faf88b80301f761abe
Myrzaim <mikosh9767@gmail.com>
2023-12-17 22:43:09 +0600

    authorization page start

---
+++ src/Components/Authorization/Autho.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Autho.css';
+import back from '../Image/back.png';
+
+const Autho = () => {
+    return (<>
+        <div className='d-flex'>
+        <div style={{ backgroundImage: `url(${back})` }}>
+              
+            </div>
+            <div></div>
+        </div>
+        </>
+    );
+};
+
+export default Autho;
