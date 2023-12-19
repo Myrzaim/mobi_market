a5e26d3b2dfaac941ab46110fd8579bb2bad650f
Myrzaim <mikosh9767@gmail.com>
2023-12-19 23:52:30 +0600

    formik start

--- src/Components/Authorization/Autho.jsx
+++ src/Components/Authorization/Autho.jsx
@@ -1,19 +1,79 @@
-import React from 'react';
-import './Autho.css';
-import back from '../Image/back.png';
-
+import React from "react";
+import "./Autho.css";
+import back from "../Image/back.png";
+import mobi from "../Image/mobi.png";
+import { Formik } from 'formik';
 const Autho = () => {
-    return (<>
-        <div className='d-flex'>
-        <div className='img_div'   style={{
+  return (
+    <>
+      <div className="d-flex-1">
+        <div
+          className="img_div"
+          style={{
               background: `url(${back})`,
-            }}>
-              <img src={back} alt="back"/>
-            </div>
-            <div></div>
+          }}
+              >
+                  <img src={mobi} alt="mobi"  className="img_mobi"/>
+                  
         </div>
-        </>
-    );
+        <div>
+              <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
+              </div>
+      </div>
+    </>
+  );
 };
 
 export default Autho;
